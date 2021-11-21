867c7e3ef99f1923d77499461cd213abe6f6c9cd
unknown <vera.colomboo@gmail.com>
2021-11-21 16:05:30 +0100

    follower.cpp

--- src/follower/follower.cpp
+++ src/follower/follower.cpp
@@ -31,10 +31,6 @@
 using namespace std;
 using namespace rapidjson;
 
-int Follower::nUpdate = 0;
-
-Follower::Follower() {}
-
 Follower::Follower(Message::node node, int nThreads) : IAgent() {
     this->nThreads = nThreads;
 
@@ -54,13 +50,13 @@ void Follower::initialize(Factory* fact) {
     }else {
         this->factory = fact;
     }
-
     if(this->storage == NULL)
+        cout << "storage == NULL" << endl;
         this->storage = this->factory->newStorage("monitoring.db");
-
-    if(this->connections == NULL)
+    if(this->connections == NULL) {
+        cout << "connections == NULL" << endl;
         this->connections = this->factory->newConnections(this->nThreads);
-    
+    }
     this->connections->initialize(this);
     this->server = this->factory->newServer(this->connections,5555);
 }
@@ -509,11 +505,14 @@ int Follower::testPing(string ip) {     // restituisce la latenza con il nodo pa
 }
 
 void Follower::getHardware() {
+
+    cout << "Follower::getHardware()" << endl;
+
     int status, i;
     sigar_t *sigar;
     sigar_cpu_t cpuT1;
     sigar_cpu_t cpuT2;
-    sigar_cpu_list_t cpulist;               // number of cores
+    sigar_cpu_list_t cpulist;
 
     sigar_open(&sigar);
     sigar_file_system_usage_t disk;
@@ -554,7 +553,7 @@ void Follower::getHardware() {
     hardware.disk = disk.total;
     hardware.mean_free_disk = disk.avail;
 
-    this->storage->saveHardware(hardware, this->node->hardwareWindow);          // save hardware data on DB
+    this->storage->saveHardware(hardware, this->node->hardwareWindow);
 
     sigar_cpu_list_destroy(sigar, &cpulist);
     sigar_close(sigar);
@@ -563,14 +562,18 @@ void Follower::getHardware() {
 void Follower::timer() {
     int iter=0;
     while(this->running) {
-
         auto t_start = std::chrono::high_resolution_clock::now();
 
         //generate hardware report and send it
         this->getHardware();
+<<<<<<< HEAD
 
         std::optional<std::pair<int64_t,Message::node>> ris = this->connections->sendUpdate(this->nodeS, this->update); // manda update al nodo leader
         if(ris == nullopt) {           // il messaggio di update non ha ottenuto ack
+=======
+        std::optional<std::pair<int64_t,Message::node>> ris = this->connections->sendUpdate(this->nodeS, this->update);
+        if(ris == nullopt) {
+>>>>>>> parent of 1966314 (aggiunta classe States)
             cout << "update retry..." << endl;
             ris = this->connections->sendUpdate(this->nodeS,this->update);  
             if(ris == nullopt) {
@@ -594,9 +597,15 @@ void Follower::timer() {
 
         //every 10 iterations ask the nodes in case the server cant reach this network
         if(iter%10 == 0) {
+<<<<<<< HEAD
             vector<Message::node> ips = this->connections->requestNodes(this->nodeS);   // chiede al Leader gli ip dei Follower nel suo gruppo
             vector<Message::node> tmp = this->getStorage()->getNodes();                 // nodi già conosciuti dal Follower
             vector<Message::node> rem;                                                  // nodi da rimuovere dai nodi conosciuti dal Follower (tmp -ips)
+=======
+            vector<Message::node> ips = this->connections->requestNodes(this->nodeS);
+            vector<Message::node> tmp = this->getStorage()->getNodes();
+            vector<Message::node> rem;
+>>>>>>> parent of 1966314 (aggiunta classe States)
 
             for(auto node : tmp) {
                 bool found = false;
@@ -611,22 +620,36 @@ void Follower::timer() {
                 }
             }
 
+<<<<<<< HEAD
             this->getStorage()->updateNodes(ips,rem);       // aggiunge nodi nuovi (ips)        
                                                             // elimina dallo storage i nodi che non vengono restituiti dal leader (rem)
+=======
+            this->getStorage()->updateNodes(ips,rem);
+>>>>>>> parent of 1966314 (aggiunta classe States)
         }
 
         //every leaderCheck iterations update the MNodes
         if(iter% this->node->leaderCheck == this->node->leaderCheck-1) {
+<<<<<<< HEAD
             vector<Message::node> res = this->connections->requestMNodes(this->nodeS);  // chiede al Leader gli ip di tutti i Leader della rete
+=======
+            vector<Message::node> res = this->connections->requestMNodes(this->nodeS);
+>>>>>>> parent of 1966314 (aggiunta classe States)
             if(!res.empty()) {
                 for(int j=0; j<res.size(); j++)
                 {
                     if(res[j].ip==std::string("::1")||res[j].ip==std::string("127.0.0.1"))
-                        res[j].ip = this->nodeS.ip;         // sostituzione ip locale del leader con ip esterno
+                        res[j].ip = this->nodeS.ip;
                 }
+<<<<<<< HEAD
                 this->node->setMNodes(res);                 // aggiornamento dei nodi leader conosciuti
                 cout << "Check server" << endl;
                 bool change = this->checkServer(res);       // controlla se c'è un nodo con latenza minore e nel caso cambia Leader/gruppo
+=======
+                this->node->setMNodes(res);
+                cout << "Check server" << endl;
+                bool change = this->checkServer(res);
+>>>>>>> parent of 1966314 (aggiunta classe States)
                 if(change) {
                     cout << "Changing server" << endl;
                     if(!selectServer(res)) {
@@ -647,7 +670,6 @@ void Follower::timer() {
         //     this->startIperf();
         // }
 
-        
         if(iter % 3 == 0) {
             string out = this->pIperf->readoutput();
             cout << "Iperf restart" << endl;
@@ -655,17 +677,19 @@ void Follower::timer() {
             this->pIperf = NULL;
             this->startIperf();
         }
-        
-        
 
         auto t_end = std::chrono::high_resolution_clock::now();
         auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(t_end-t_start).count();
-        int sleeptime = this->node->timeReport-elapsed_time;
+<<<<<<< HEAD
+        int sleeptime = Node::timeReport-elapsed_time;
         
         
-        //cout << "Time report: " << this->node->timeReport << endl;
+        cout << "Time report: " << Node::timeReport << endl;
         
 
+=======
+        int sleeptime = this->node->timeReport-elapsed_time;
+>>>>>>> parent of 1966314 (aggiunta classe States)
         if (sleeptime > 0)
             sleeper.sleepFor(chrono::seconds(sleeptime));
 
@@ -714,7 +738,7 @@ void Follower::TestTimer() {
             LatencyThreads.push_back(move(LatencyThread));
         }
 
-        
+
         //start thread for bandwidth tests
         thread BandwidthThread = thread([this]{
             //test bandwidth
@@ -722,7 +746,7 @@ void Follower::TestTimer() {
             vector<Message::node> ips = this->storage->getLRBandwidth(this->node->maxPerBandwidth + 5, this->node->timeBandwidth);
             cout << "List B: ";
             for(auto node : ips) {
-                //cout << node.ip << " ";
+                cout << node.ip << " ";
             }
             cout << endl;
             int i=0;
@@ -746,16 +770,13 @@ void Follower::TestTimer() {
                 i++;
             }
         });
-        
-    
 
         for(auto &LatencyThread : LatencyThreads) {
             LatencyThread.join();
         }
         
-        
         BandwidthThread.join();
-        
+
 
         sleeper.sleepFor(chrono::seconds(this->node->timeTests));
         iter++;
