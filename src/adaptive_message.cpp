d25fad873b96169c509aa37d89792784f0bcd485
unknown <vera.colomboo@gmail.com>
2021-11-21 15:57:00 +0100

    revert11

--- src/adaptive_message.cpp
+++
@@ -1,9 +0,0 @@
-#include "adaptive_message.hpp"
-#include "adaptive_report.hpp"
-
-AdaptiveMessage::AdaptiveMessage() : Message() { }
-AdaptiveMessage::~AdaptiveMessage() { }
-
-void AdaptiveMessage::setData(AdaptiveReport& report) {
-    this->data = *(report.getJson());
-}
--- src/leader/adaptive_leader_factory.cpp
+++
@@ -1,7 +0,0 @@
-#include "adaptive_leader_factory.hpp"
-#include <iostream>
-
-AdaptiveLeaderConnections* AdaptiveLeaderFactory::newConnections(int nThread){
-    cout << "AdaptiveLeaderFactory::newConnections()" << endl;
-    return new AdaptiveLeaderConnections(nThread);
-}
